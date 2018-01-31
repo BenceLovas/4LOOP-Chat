30f7eda00ac905137d9ea765f8b2c236a21a0741
godofdeath11 <ezmegaz11@gmail.com>
2018-01-31 18:18:21 +0100

    some front end implementation for texting

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,5 @@
 $(function() {
-    
+
     function getUsers() {
         $.ajax({
             type: "GET",
@@ -17,13 +17,50 @@ $(function() {
 
 
     $('#getUsersButton').click(getUsers);
-    
+
 });
-function getUser1() {
-    $.ajax({
-        type: "GET",
-        url: "/getUser1",
-        data:data,
-        success: console.log(data)
-    })
+	function getMessages() {
+		$.ajax({
+			url: "/get-messages",
+			dataType: "json",
+			type: "GET",
+			success: function(data) {
+                var targetTable = document.getElementById("chattable")
+                targetTable.innerHTML = ''
+                for(var i = 0;i < data.length;i++){
+                    var newtd = document.createElement("td")
+                    var newtr = document.createElement("tr")
+                    newtr.appendChild(newtd)
+                    newtd.innerHTML += data[i].message
+                    targetTable.appendChild(newtr)
+                }
+                var tdList = document.getElementsByTagName("td")
+                for(var j = 0;j < tdList.length;j++){
+                    if(j % 2 == 0){
+                    tdList[j].className ="red";
+                    }else{
+                        tdList[j].className = "blue";
+                    }
+			    }
+	        }
+        })
+    }
+
+    function sendAjax() {
+        $.ajax({
+            url: '/write_message',                //function route to give the data to
+            type: 'POST',                       //methods =['POST'] must be added to the function in the server.py
+            data: {                             //data must be an object a json format object
+                'message': $( '#new_message' ).val(),    //getting input field value
+            },
+            success: function(){                //on success function
+                $( '#new_message' ).val('')
+                getMessages()
+            }
+        })
+    }
+
+function sayhello(){
+console.log("hello)")
 }
+
