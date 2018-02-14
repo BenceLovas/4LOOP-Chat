db61f304ac75be7f39387205e66b544194940bc3
Bence Lovas <bansoul1@gmail.com>
2018-02-14 10:50:56 +0100

    unique constraint to channel users

--- src/main/resources/static/script/channelList.js
+++ src/main/resources/static/script/channelList.js
@@ -4,33 +4,34 @@ var channelListController = {
             type: "GET",
             url: "/get-all-channels",
             success: response => {
-            $("#main_window").html("");
-            response.channels.forEach(function (channel){
-                let div = $("<div/>", {});
-                let name = $("<p/>").text(channel.name);
-                name.attr("class", "channelList");
-                let userSize = $("<p/>").text("size of the channel : " + channel.userList.length);
-                userSize.attr("class", "channelList");
-                let joinButton = $("<button/>");
-                div.attr("data-id", channel.id);
-                joinButton.click(function(){
-                    let data = {"channelId": $(this).parent().data("id")};
-                    console.log(data);
-                    $.ajax({
-                        type: "POST",
-                        url: "/add-user-to-channel",
-                        data: data,
-                        success:channelController.populateChannelList,
-                        error: console.log("error")
+                $("#main_window").html("");
+                response.channels.forEach(function (channel){
+                    let div = $("<div/>", {});
+                    let name = $("<p/>").text(channel.name);
+                    name.attr("class", "channelList");
+                    let userSize = $("<p/>").text("size of the channel : " + channel.userList.length);
+                    userSize.attr("class", "channelList");
+                    let joinButton = $("<button/>");
+                    div.attr("data-id", channel.id);
+                    joinButton.click(function(){
+                        let data = {"channelId": $(this).parent().data("id")};
+                        $.ajax({
+                            type: "POST",
+                            url: "/add-user-to-channel",
+                            data: data,
+                            success: channelController.populateChannelList,
+                            error: response => {
+                                console.log("error");
+                            }
+                        });
                     });
+                    joinButton.text("Join channel");
+                    div.append(name);
+                    div.append(userSize);
+                    div.append(joinButton);
+                    $("#main_window").append(div);
                 });
-                joinButton.text("Join channel");
-                div.append(name);
-                div.append(userSize);
-                div.append(joinButton);
-                $("#main_window").append(div);
-                });
-                }
-            });
-        },
-}
+            }
+        });
+    },
+};
