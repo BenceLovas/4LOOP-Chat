be23ba5dd02d8c54d9e63641afb98a73ff92aa47
godofdeath11 <ezmegaz11@gmail.com>
2018-02-26 12:18:48 +0100

    Cannot login or register with an empty input field

--- src/main/resources/static/script/login.js
+++ src/main/resources/static/script/login.js
@@ -1,35 +1,59 @@
 $(function () {
 
     function postRegistrationData(event) {
-        event.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "/user/registration",
-            data: $('#registrationForm').serialize(),
-            success: response => {
-                window.location.replace(response.redirect);
-            },
-            error: response => {
-                $('#registrationError').text(response.responseJSON.response);
+        let validInput = true;
+        $.each($('#registrationForm').serializeArray(), function (index, inputField) {
+            if(inputField.value == "" && validInput == true){
+                console.log(inputField.name + "is empty");
+                $("#loginError").html(inputField.name + " is empty")
+                validInput = false;
             }
         });
+
+        event.preventDefault();
+        if(validInput == true) {
+            $.ajax({
+                type: "POST",
+                url: "/user/registration",
+                data: $('#registrationForm').serialize(),
+                success: response => {
+                    window.location.replace(response.redirect);
+                },
+                error: response => {
+                    $('#registrationError').text(response.responseJSON.response);
+                }
+            });
+        }
     }
 
     $('#registrationButton').click(postRegistrationData);
 
     function postLoginData(event) {
-        event.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "/user/login",
-            data: $('#loginForm').serialize(),
-            success: response => {
-                window.location.replace(response.redirect);
-            },
-            error: response => {
-                $('#loginError').text(response.responseJSON.response);
-            }
+
+        let validInput = true;
+        $.each($('#loginForm').serializeArray(), function (index, inputField) {
+            if(inputField.value == "" && validInput == true){
+               console.log(inputField.name + "is empty");
+                $("#loginError").html(inputField.name + " is empty")
+                validInput = false;
+           }
         });
+
+
+        event.preventDefault();
+        if(validInput == true) {
+            $.ajax({
+                type: "POST",
+                url: "/user/login",
+                data: $('#loginForm').serialize(),
+                success: response => {
+                    window.location.replace(response.redirect);
+                },
+                error: response => {
+                    $('#loginError').text(response.responseJSON.response);
+                }
+            });
+        }
     }
 
     $('#loginButton').click(postLoginData);
