0d794d3e620a888c46439608963ac9bef91a7303
Henrik Miszoglad <henrik.miszoglad@gmail.com>
2018-02-28 14:37:25 +0100

    hash salt

--- src/main/java/com/forloop/service/UserService.java
+++ src/main/java/com/forloop/service/UserService.java
@@ -5,6 +5,7 @@ import com.forloop.dao.UserDAOJPA;
 import com.forloop.exceptions.NameAlreadyTakenException;
 import com.forloop.model.User;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.*;
 
 @Service
 public class UserService {
@@ -18,15 +19,24 @@ public class UserService {
     public User registration(User user) throws NameAlreadyTakenException {
         userDAOJPA.save(user);
         return user;
-        //return dao.insertUser(user);
     }
 
     public User login(String username, String password) {
 
         User userInDB = userDAOJPA.getByName(username);
-        if (userInDB != null && password.equals(userInDB.getPassword())) {
+        if (userInDB != null && checkPw(userInDB, password)) {
             return userInDB;
         }
         return null;
+    }
+
+    public String encryptPw(String pw){
+        return BCrypt.hashpw(pw, BCrypt.gensalt());
+    }
+
+
+    private boolean checkPw(User user, String pw){
+        String storedPw = user.getPassword();
+        return BCrypt.checkpw(pw, storedPw);
     }
 }
